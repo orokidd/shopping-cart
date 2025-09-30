9bf8aed0bdd271b12a1d27727874c6bf14999121
orokidd <gregomily@gmail.com>
2025-09-30 17:48:45 +0800

    setup folders

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
