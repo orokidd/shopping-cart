b39ecb28595604a1605b63273e00e250dea856aa
orokidd <gregomily@gmail.com>
2025-10-03 14:47:19 +0800

    made header, created root layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header>
+      <button className="shop-button">Shop</button>
+      <button className="home-button">Home</button>
+      <button className="cart-button">Cart</button>
+    </header>
+  );
+}
