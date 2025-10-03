a5eeb627ae4dddeed4abb3fb29add903f61e6d89
orokidd <gregomily@gmail.com>
2025-10-03 15:53:19 +0800

    added hero image

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import hero from "../assets/hero-1.jpg";
+
+export default function Hero() {
+  return (
+    <div className="hero-container">
+      <img className="hero-img" src={hero} alt="Hero" />
+    </div>
+  );
+}
