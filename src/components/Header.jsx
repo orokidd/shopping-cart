2b5a76daf23eaa76bee885b464e8c5ca28fcab64
orokidd <gregomily@gmail.com>
2025-10-03 16:04:06 +0800

    added custom font, change home link text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export function Header() {
   return (
     <header className="header">
-      <Link to="/shop" className="shop-button">Shop</Link>
-      <Link to="/" className="home-button">Home</Link>
-      <Link to="/cart" className="cart-button">Cart</Link>
+      <Link to="/shop" className="shop-link">Shop</Link>
+      <Link to="/" className="home-link">TERATAI</Link>
+      <Link to="/cart" className="cart-link">Cart</Link>
     </header>
   );
 }
