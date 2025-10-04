ec07d2572991802d193c1b92f489fa193e4c0429
orokidd <gregomily@gmail.com>
2025-10-04 14:59:08 +0800

    new arrivals product image

--- src/components/NewArrivals.jsx
+++ src/components/NewArrivals.jsx
@@ -2,14 +2,16 @@ import { useLoaderData } from "react-router-dom";
 
 export default function NewArrivals() {
   const { data } = useLoaderData();
+  const newProducts = [data[4], data[5], data[6]];
 
   return (
     <section className="new-arrivals">
       <h2 className="new-arrivals-title">New Arrivals</h2>
-      <p>Check out the latest products in our collection!</p>
+      <p className="new-arrivals-description">Check out the latest products in our collection!</p>
       <div className="product-list">
-        {data.map((product) => (
+        {newProducts.map((product) => (
           <div key={product.id} className="product-item">
+            <img src={product.image} alt={product.title} className="product-image" />
             <h3>{product.title}</h3>
             <p>${product.price}</p>
           </div>
