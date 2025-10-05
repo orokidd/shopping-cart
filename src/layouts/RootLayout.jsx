aeacd7981ba5ed43f6e8f349b114da4e8ca33979
orokidd <gregomily@gmail.com>
2025-10-05 12:24:46 +0800

    added context for cart items

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header";
+import { CartProvider } from "../contexts/CartContext";
 
 export default function RootLayout() {
   return (
-    <>
+    <CartProvider>
       <Header />
       <main>
         <Outlet />
       </main>
-    </>
+    </CartProvider>
   );
 }
