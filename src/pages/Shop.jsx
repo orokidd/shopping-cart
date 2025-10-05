e3937bd48b328ad73bd385143981d8baa83d5c82
orokidd <gregomily@gmail.com>
2025-10-05 11:03:25 +0800

    clickable link for each product in shop page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,11 +1,10 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Link } from "react-router-dom";
 
 export default function Shop() {
   const { data } = useLoaderData();
 
   return (
     <section className="shop-page">
-
       <div className="category">
         <p>All products</p>
         <p>Men</p>
@@ -16,7 +15,9 @@ export default function Shop() {
       <div className="product-list shop">
         {data.map((product) => (
           <div className="product-item" key={product.id}>
-            <img src={product.image} alt={product.name} />
+            <Link to={`/product/${product.id}`}>
+              <img src={product.image} alt={product.name} />
+            </Link>
             <h3 className="product-title">{product.title}</h3>
             <p className="product-price">${product.price}</p>
           </div>
