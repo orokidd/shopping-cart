b5db0c449e4e1c17dc34648b2d57470e0b0b8c32
orokidd <gregomily@gmail.com>
2025-10-09 22:00:13 +0800

    hide decrease quantity button if the current quantity is 1

--- src/components/Product/Quantity.jsx
+++ src/components/Product/Quantity.jsx
@@ -2,7 +2,9 @@ export default function Quantity({ quantity, setQuantity }) {
   return (
     <div className="quantity-selector">
       <p>Quantity:</p>
-      <button className="decrease-btn" onClick={() => setQuantity(quantity - 1)}>-</button>
+      
+      { quantity > 1 ? <button className="decrease-btn" onClick={() => setQuantity(quantity - 1)}>-</button> : null}
+      
       <span className="quantity-value">{quantity}</span>
       <button className="increase-btn" onClick={() => setQuantity(quantity + 1)}>+</button>
     </div>
