e7dfb8bcc5836a953c4823f87d60a160d7a41c78
orokidd <gregomily@gmail.com>
2025-10-09 22:06:45 +0800

    styling for quantity selector

--- src/components/Product/Quantity.jsx
+++ src/components/Product/Quantity.jsx
@@ -2,11 +2,14 @@ export default function Quantity({ quantity, setQuantity }) {
   return (
     <div className="quantity-selector">
       <p>Quantity:</p>
-      
-      { quantity > 1 ? <button className="decrease-btn" onClick={() => setQuantity(quantity - 1)}>-</button> : null}
-      
-      <span className="quantity-value">{quantity}</span>
-      <button className="increase-btn" onClick={() => setQuantity(quantity + 1)}>+</button>
+
+      <div className="quantity-buttons">
+        {quantity > 1 ? (
+          <button className="decrease-btn" onClick={() => setQuantity(quantity - 1)}>-</button>
+        ) : null}
+        <span className="quantity-value">{quantity}</span>
+        <button className="increase-btn" onClick={() => setQuantity(quantity + 1)}>+</button>
+      </div>
     </div>
-  )
+  );
 }
