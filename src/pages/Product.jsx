bd0cfd8dbd716ea301ef9df37c999c9aec381fbb
orokidd <gregomily@gmail.com>
2026-01-03 12:44:28 +0800

    remove unused imports

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,5 @@
 import { useLoaderData, useParams } from "react-router-dom";
-import { AddCartButton } from "../components/Product/AddCartButton";
 import ProductImage from "../components/Product/ProductImage";
-import { useState } from "react";
-import Quantity from "../components/Product/Quantity";
 import ProductDetail from "../components/Product/ProductDetail";
 
 export default function Product() {
